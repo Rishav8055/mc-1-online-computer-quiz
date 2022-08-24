fb217ed3bd42b153f4a707160faba72c69980c53
rishavemail786 <rishavemail786@gmail.com>
2022-08-24 09:38:27 +0530

    SOLUTION DONE

--- src/main/java/com/jap/QuizDemo.java
+++ src/main/java/com/jap/QuizDemo.java
@@ -2,19 +2,41 @@ package com.jap;
 
 public class QuizDemo {
 
+
     //return the school name which has got the highest score in quiz
     //Handle the NumberFormatException
     public String highestScore(String []nameOfSchool , String[] scores ){
+        int highestScoreIndex=0;
+        try {
+            int maxElement=Integer.parseInt(scores[0]);
+            for (int index=0;index<scores.length;index++){
+                if (Integer.parseInt(scores[index])>maxElement){
+                    maxElement=Integer.parseInt(scores[index]);
+                    highestScoreIndex=index;
+                }
+            }
+        }catch (NumberFormatException e){
+            return e.toString();
+        }
 
 
-        return "";
+        return nameOfSchool[highestScoreIndex];
 
     }
     //convert all the names in uppercase
     //Handle the NullPointerException
     public String[] convertAllNamesToCapital(String name[]){
-        String upperCase[] = new String[0];
+        String upperCase[] = new String[name.length];
+        try {
+            for (int index=0;index< upperCase.length;index++){
+                upperCase[index]=name[index].toUpperCase();
 
+            }
+        }catch (NullPointerException exception) {
+            String[] nullPointer= new String[1];
+            nullPointer[0]=exception.toString();
+            return nullPointer;
+        }
         return upperCase;
     }
 }
